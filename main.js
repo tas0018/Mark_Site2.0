7f312f6eddbde3b9ec1e5603856bce443dffc2f1
Taylor Smith <tas0018@auburn.edu>
2021-03-21 00:26:40 -0500

    Finished styling contact section

--- main.js
+++ main.js
@@ -43,6 +43,41 @@ btns.forEach((btn, i) => {
     });
 });
 
+/*Dot Nov code */
+$(window).scroll(function(){
+    var scrollPosition = $(window).scrollTop();
+    var dotOne = $('#dot-one').offset().top;
+    var dotTwo = $('#dot-two').offset().top;
+    var dotThree = $('#dot-three').offset().top;
+    var dotFour =$('dot-four').offset().top;
 
+    var nav1 = $('a[href="#dot-one"]');
+    var nav2 = $('a[href="#dot-two"]');
+    var nav3 = $('a[href="#dot-three"]');
+    var nav4 = $('a[href="#dot-four"]');
+
+    if (scrollPosition >= dotOne){
+        $('.activeNav').removeClass('activeNav');
+        $('#nav1').addClass('activeNav');
+        nav1.contents('li').text('Section 1');
+        nav1.siblings().contents('li').text('');
+    }
+
+    if (scrollPosition >= dotTwo){
+        $('.activeNav').removeClass('activeNav');
+        $('#nav2').addClass('activeNav');
+        nav2.contents('li').text('Section 2');
+        nav2.siblings().contents('li').text('');
+    }
+
+    if (scrollPosition >= dotThree){
+        $('.activeNav').removeClass('activeNav');
+        $('#nav3').addClass('activeNav');
+        nav1.contents('li').text('Section 3');
+        nav1.siblings().contents('li').text('');
+    }
+
+
+});
 
 
