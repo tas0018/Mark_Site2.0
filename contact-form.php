bbd08eea64e403a39efa2a9fe657458ef98ed620
Taylor Smith <tas0018@auburn.edu>
2021-06-18 10:34:22 -0500

    testing email

---
+++ contact-form.php
@@ -0,0 +1,16 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+    $subject = "Apogee Site Email"
+
+    $mailTo = "tas0018@auburn.edu"
+    $headers = "From: ".$mailFrom;
+    $txt =  "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
+?>
