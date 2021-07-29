21d3450f4ed683db121774b483e8d711b7c5a952
Taylor Smith <tas0018@auburn.edu>
2021-07-29 10:50:20 -0500

    Added swipe functionality on the slide sections

--- main.js
+++ main.js
@@ -1,64 +1,162 @@
-const tabs = document.querySelectorAll(['[data-tab-target]'])
-const tabContents = document.querySelectorAll('[data-tab-content]')
-
+const tabs = document.querySelectorAll(["[data-tab-target]"]);
+const tabContents = document.querySelectorAll("[data-tab-content]");
 
 //Smooth scroll using jquery
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
 
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
-        })
-
-        target.classList.add('active')
-    })
-})
+    target.classList.add("active");
+  });
+});
 
 //jquery code move to top of PoweEd section on click
 $(".btn").click(function () {
-    $('html,body').animate({
-        scrollTop: $(".img-slider").offset().top
+  $("html,body").animate(
+    {
+      scrollTop: $(".img-slider").offset().top,
     },
-        'slow');
+    "slow"
+  );
 });
 
-const circle = document.querySelector('#circle-one');
-console.log()
+//Jquery code for swiping functionality for slide 1
+$("#power-slide").on("swipeleft", function (e) {
+  currentSlide = 0;
+  slides.forEach((slide) => {
+    slide.classList.remove("activeSlide");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("activeSlide");
+    });
+  });
+
+  slides[currentSlide + 1].classList.add("activeSlide");
+  btns[currentSlide + 1].classList.add("activeSlide");
+  currentSlide = 1;
+  console.log(currentSlide);
+  console.log(screenSize);
+});
+
+//Jquery code for swiping functionality for slide 2
+//Swipe left
+$("#purchase-slide").on("swipeleft", function (e) {
+  slides.forEach((slide) => {
+    slide.classList.remove("activeSlide");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("activeSlide");
+    });
+  });
+
+  slides[currentSlide + 1].classList.add("activeSlide");
+  btns[currentSlide + 1].classList.add("activeSlide");
+  currentSlide++;
+  console.log(currentSlide);
+});
+
+//Swipe Right
+$("#purchase-slide").on("swiperight", function (e) {
+  slides.forEach((slide) => {
+    slide.classList.remove("activeSlide");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("activeSlide");
+    });
+  });
+
+  slides[currentSlide - 1].classList.add("activeSlide");
+  btns[currentSlide - 1].classList.add("activeSlide");
+  currentSlide--;
+  console.log(currentSlide);
+});
+
+//Jquery code for swiping functionality for slide 3
+//Swipe left
+$("#lease-slide").on("swipeleft", function (e) {
+  slides.forEach((slide) => {
+    slide.classList.remove("activeSlide");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("activeSlide");
+    });
+  });
+
+  slides[currentSlide + 1].classList.add("activeSlide");
+  btns[currentSlide + 1].classList.add("activeSlide");
+  currentSlide++;
+  console.log(currentSlide);
+});
+
+//Swipe Right
+$("#lease-slide").on("swiperight", function (e) {
+  slides.forEach((slide) => {
+    slide.classList.remove("activeSlide");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("activeSlide");
+    });
+  });
+
+  slides[currentSlide - 1].classList.add("activeSlide");
+  btns[currentSlide - 1].classList.add("activeSlide");
+  currentSlide--;
+  console.log(currentSlide);
+});
+
+//Jquery code for swiping functionality for slide 4
+//Swipe right
+$("#subscribe-slide").on("swiperight", function (e) {
+  slides.forEach((slide) => {
+    slide.classList.remove("activeSlide");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("activeSlide");
+    });
+  });
+
+  slides[currentSlide - 1].classList.add("activeSlide");
+  btns[currentSlide - 1].classList.add("activeSlide");
+  currentSlide--;
+  console.log(currentSlide);
+});
+
+const circle = document.querySelector("#circle-one");
+console.log();
 
 /*Slide Section code */
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btn");
 let currentSlide = 1;
 
 // javacript slider manual navigation
 var manualNav = function (manual) {
-    slides.forEach((slide) => {
-        slide.classList.remove('activeSlide');
+  slides.forEach((slide) => {
+    slide.classList.remove("activeSlide");
 
-        btns.forEach((btn) => {
-            btn.classList.remove('activeSlide');
-        });
+    btns.forEach((btn) => {
+      btn.classList.remove("activeSlide");
     });
+  });
 
-    slides[manual].classList.add('activeSlide');
-    btns[manual].classList.add('activeSlide')
-}
+  slides[manual].classList.add("activeSlide");
+  btns[manual].classList.add("activeSlide");
+};
 
 btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        manualNav(i);
-        currentSlide = i;
-    });
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
 });
 
 /*Sticky Nav code Starts*/
 window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 /*Sticky Nav code Ends*/
-
-
